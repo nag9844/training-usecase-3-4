b36abb83c8bf42bbeba0aa4b2ec3c7ebcec1d5fb
nag9844 <vnagaraj984@gmail.com>
2025-06-03 23:31:04 +0530

    backend details update

--- modules/openproject/main.tf
+++ modules/openproject/main.tf
@@ -8,39 +8,13 @@ resource "aws_instance" "openproject" {
   #user_data = templatefile("${path.module}/user_data.sh", {})
 
   user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update -y
-    curl -fsSL https://get.docker.com -o install-docker.sh
-    sudo sh install-docker.sh
-    sudo usermod -aG docker ubuntu
-    mkdir -p /home/ubuntu/openproject
-    cd /home/ubuntu/openproject
-    cat > docker-compose.yml << 'EOL'
-    version: '3'
-
-    services:
-      openproject:
-        image: openproject/community:latest
-        ports:
-          - "8080:8080"
-        environment:
-          - DATABASE_URL=mysql2://root:secret@mysql:3306/openproject
-        depends_on:
-          - mysql
-
-      mysql:
-        image: mysql:5.7
-        environment:
-          MYSQL_ROOT_PASSWORD: secret
-        volumes:
-          - db_data:/var/lib/mysql
-
-    volumes:
-      db_data:
-    EOL
-
-    docker-compose up -d
-  EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt-get install -y docker.io
+                sudo systemctl start docker
+                sudo systemctl enable docker
+                sudo docker run -d -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HTTPS=false openproject/openproject:15.4.1
+                EOF
 
   tags = {
     Name = "openproject-instance"
