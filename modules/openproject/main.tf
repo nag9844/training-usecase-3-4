fa74120f64bf2f87bd28801172b41bd2400432c0
nag9844 <vnagaraj984@gmail.com>
2025-06-03 22:05:29 +0530

    backend details update

--- modules/openproject/main.tf
+++ modules/openproject/main.tf
@@ -5,15 +5,27 @@ resource "aws_instance" "openproject" {
   vpc_security_group_ids = var.security_group_ids
   subnet_id              = var.public_subnet_ids[0]
 
-  # user_data = templatefile("${path.module}/user_data.sh", {})
+  # # user_data = templatefile("${path.module}/user_data.sh", {})
+  # user_data = <<-EOF
+  #             #!/bin/bash
+  #             sudo apt update -y
+  #             curl -fsSL https://get.docker.com -o install-docker.sh
+  #             sudo sh install-docker.sh
+  #             sudo usermod -aG docker ubuntu
+  #             docker run -d -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HTTPS=false openproject/openproject:15.4.1
+  #             EOF
+
   user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y
-              curl -fsSL https://get.docker.com -o install-docker.sh
-              sudo sh install-docker.sh
-              sudo usermod -aG docker ubuntu
-              docker run -d -p 80:80 -e OPENPROJECT_SECRET_KEY_BASE=secret -e OPENPROJECT_HTTPS=false openproject/openproject:15.4.1
-              EOF
+    #!/bin/bash
+    sudo apt update -y
+    curl -fsSL https://get.docker.com -o install-docker.sh
+    sudo sh install-docker.sh
+    sudo usermod -aG docker ubuntu
+    docker run -d -p 80:80 \
+     -e OPENPROJECT_SECRET_KEY_BASE=secret \
+     -e OPENPROJECT_HTTPS=false \
+     openproject/openproject:15.4.1
+  EOF
 
   tags = {
     Name = "openproject-instance"
